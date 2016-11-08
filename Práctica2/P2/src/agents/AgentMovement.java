cede9185c851c03b3c3214c53a66abfa3a582ccf
Unknown <rodriguez_bueno@hotmail.com>
2016-11-08 14:42:18 +0100

    Clases AgentMovement (heurística simple) y AgentScanner
    Heurística: sigue el menor valor dado por el scanner

---
+++ Práctica2/P2/src/agents/AgentMovement.java
@@ -0,0 +1,275 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agents;
+
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+import es.upv.dsic.gti_ia.core.AgentID;
+
+/**
+ *
+ * @author rodri
+ */
+public class AgentMovement extends Agent{
+    private static final int IDLE = 0;
+    private static final int WAIT_SCANNER = 1;
+    private static final int WAIT_WORLD = 2;
+    private static final int EXECUTE = 3;
+    private static final int FINISH = 4;
+    
+    private int state;
+    private boolean finish;
+    private int x,y;
+    private static final int WIDTH = 500, HEIGHT = 500;
+    private float [][] map_scanner = new float [WIDTH][HEIGHT];
+    private int [][] map_world = new int [WIDTH][HEIGHT];
+    
+    private JsonObject responseObject;
+    
+    private String world;
+    private String movementName;
+    private String scannerName;
+    private String carName;
+    
+    String message;
+    
+    /**
+     * Constructor 
+     * @param aid El ID de agente para crearlo.
+     * 
+     * @throws java.lang.Exception en la creaciÃ³n del agente.
+     * 
+     * @author AarÃ³n RodrÃ­guez Bueno
+     */
+    public AgentMovement(AgentID aid,String movementName, String world, String scannerName, String carName) throws Exception {
+            super(aid);
+            this.scannerName= scannerName;
+            this.world = world;
+            this.scannerName = scannerName;
+            this.carName = carName;
+
+    }
+    
+    /**
+     * MÃ©todo de inicializaciÃ³n del Agent Movement
+     * 
+     * @author AarÃ³n RodrÃ­guez Bueno
+     */
+    @Override
+    public void init(){
+        state = IDLE;
+        finish = false;
+        
+        this.responseObject = new JsonObject();
+        
+        /*for (float [] i : map_scanner){
+            for(float j : i){
+                j = 0.0f;
+            }
+        }
+        for (float [] i : map_world){
+            for(float j : i){
+                j = 0;
+            }
+        }
+        x = y = -1;
+        */
+    }
+    
+    
+    /**
+    * MÃ©todo de ejecuciÃ³n del agente Scanner
+    * 
+    * @author AarÃ³n RodrÃ­guez Bueno
+    */  
+    @Override
+    public void execute() {
+        while(!finish){
+            switch(state){
+                case IDLE:  //Esperando al proceed del Agent Car
+                    
+                    /*
+                    message = this.receiveMessage("*");
+                    if(message.contains("CRASHED")||message.contains("logout")) 
+                        state = finish;
+                    else{
+                        ...
+                    }
+                    break;
+                    */
+                    message = this.receiveMessage(carName);
+                    if (message.contains("ok")){
+                        //Le pedimos los datos al Agent Scanner
+                        responseObject = new JsonObject(); //Lo limpiamos
+                        responseObject.add(movementName, "ok");
+                        message = responseObject.toString();
+                        sendMessage(scannerName, message);
+                        
+                        state = WAIT_SCANNER;
+                    }
+                    else{
+                        state = FINISH;   
+                    }
+                    
+                    
+                    break;
+               
+                
+                
+                
+                
+                
+                case WAIT_SCANNER:  //Esperando los datos del Agent Scanner
+                    
+                    /*
+                    message = this.receiveMessage("*");
+                    if(message.contains("CRASHED")||message.contains("logout")) 
+                        state = finish;
+                    else{
+                        ...
+                    }
+                    break;
+                    */
+                    message = this.receiveMessage(scannerName);
+                    this.responseObject = Json.parse(message).asObject();
+                    
+                    //Actualizamos nuestro map_scanner
+                    int posx = 0;
+                    int posy = 0;
+                    for (JsonValue j : responseObject.get("scanner").asArray()){
+                        map_scanner[posx][posy] = j.asFloat();
+                        posy++;
+                        if(posy%WIDTH == 0){
+                            posy = 0;
+                            posx++;
+                        }
+                    }
+                    
+                    //Pedimos al Agent World los datos
+                    responseObject = new JsonObject(); //Lo limpiamos
+                    responseObject.add(movementName, "ok");
+                    message = responseObject.toString();
+                    sendMessage(world, message);
+                    
+                    state = WAIT_WORLD;
+                    break;
+                
+                
+                
+                
+                
+                
+                
+                case WAIT_WORLD:    //Esperando los datos del Agent World
+                    
+                    /*
+                    message = this.receiveMessage("*");
+                    if(message.contains("CRASHED")||message.contains("logout")) 
+                        state = finish;
+                    else{
+                        ...
+                    }
+                    break;
+                    */
+                    message = this.receiveMessage(world);
+                    this.responseObject = Json.parse(message).asObject();
+                    
+                    //Actualizamos nuestro world_scanner
+                    this.x = responseObject.get("x").asInt();
+                    this.y = responseObject.get("y").asInt();
+                    int posix = 0, posiy = 0;
+                    for (JsonValue j : responseObject.get("world").asArray()){
+                        map_world[posix][posiy] = j.asInt();
+                        posiy++;
+                        if(posiy%WIDTH == 0){
+                            posiy = 0;
+                            posix++;
+                        }
+                    }
+                    
+                    
+                    state = EXECUTE;
+                    break;
+                
+                
+                
+                
+                
+                
+                
+                case EXECUTE:   //Calcula la decisiÃ³n y se la manda al Agent Car
+                    
+                    //Una vez tenemos todos los datos, calculamos el mejor movimiento
+                    //PRIMERA HEURISTICA: movernos sÃ³lo en direcciÃ³n al goal en lÃ­nea recta
+                    float menor = map_scanner[x-1][y-1]+1;
+                    int nuevax, nuevay;
+                                       
+                    for (int i = x-1; i <= x+1; i++){
+                        for (int j = y-1; j <= y+1; j++){
+                            if (menor > map_scanner[i][j]){ //No miro que si x!=i o y!=j porque si no estamos ya en el goal, 
+                                                            //siempre habrÃ¡ un borde con menos valor que la posiciÃ³n en la que estamos actualmente
+                               nuevax = i;
+                               nuevay = j;
+                            }
+                        }
+                    }
+                    
+                    responseObject = new JsonObject(); //Lo limpiamos
+                    //Comprobamos quÃ© posiciÃ³n respecto a nuestra posiciÃ³n es la que hemos elegido
+                    if (nuevax == x-1){
+                        if (nuevay == y-1)
+                            responseObject.add("movement", "NO");
+                        else if (nuevay == y)
+                            responseObject.add("movement", "N");
+                        else
+                            responseObject.add("movement", "NE");
+                    }
+                    else if (nuevax == x){
+                        if (nuevay == y-1)
+                            responseObject.add("movement", "O");
+                        else
+                            responseObject.add("movement", "E");
+                    }
+                    else{
+                        if (nuevay == y-1)
+                            responseObject.add("movement", "SO");
+                        else if (nuevay == y)
+                            responseObject.add("movement", "S");
+                        else
+                            responseObject.add("movement", "SE");
+                    }
+                    
+                    message = responseObject.toString();
+                    sendMessage(carName, message);
+                    
+                    state = IDLE;
+                    break;
+                
+                
+                
+                
+                
+                
+                
+                case FINISH:    //Matamos al agente
+                    this.finalize();
+                    break;
+            }
+        }
+    }
+    
+    
+    /**
+    * MÃ©todo de finalizaciÃ³n del Agent Movement
+     * 
+     * @author AarÃ³n RodrÃ­guez Bueno
+    */
+    @Override
+    public void finalize() {
+        super.finalize();
+    }
+}
---
+++ Práctica2/P2/src/agents/AgentScanner.java
@@ -0,0 +1,217 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agents;
+
+import agents.Agent;
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.SingleAgent;
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ * Clase que define al agente Scanner, el cual se va a encargar de controlar los datos relacionados
+ * con el scanner dados por el servidor y compartir su informaciÃ³n con el AgentWorld.
+ * 
+ * @author AarÃ³n RodrÃ­guez Bueno
+ */
+public class AgentScanner extends Agent {
+    
+    //Variables de estado
+    private static final int IDLE = 0;
+    private static final int WAIT_GPS = 1;
+    private static final int WAIT_MOVEMENT = 2;
+    private static final int FINISH = 3;
+    private static final int WIDTH = 500;
+    private static int HEIGHT = 500;
+    
+    private int state;
+    private float [][] map_scanner = new float [WIDTH][HEIGHT];
+    private float [] local_scanner = new float [25];
+    private boolean finish;
+    private JsonObject responseObject;
+    
+    private String movementName;
+    private String scannerName;
+    private String carName;
+    private String gpsName;
+    
+    /**
+     * Constructor 
+     * @param aid El ID de agente para crearlo.
+     * 
+     * @throws java.lang.Exception en la creaciÃ³n del agente.
+     * 
+     * @author AarÃ³n RodrÃ­guez Bueno
+     */
+    public AgentScanner(AgentID aid,String scannerName,String movementName,String gpsName) throws Exception {
+        super(aid);
+        this.movementName = movementName;
+        this.scannerName = scannerName;
+        this.gpsName = gpsName;
+
+    }
+    
+    
+    /**
+     * MÃ©todo de inicializaciÃ³n del Agent Scanner
+     * 
+     * @author AarÃ³n RodrÃ­guez Bueno
+     */
+    @Override
+    public void init(){
+        state = IDLE;
+        finish = false;
+        
+        this.responseObject = new JsonObject();
+        
+        /*for (float [] i : map_scanner){
+            for(float j : i){
+                j = 0;
+            }
+        }*/
+    }
+    
+    /**
+    * MÃ©todo de ejecuciÃ³n del agente Scanner
+    * 
+    * @author AarÃ³n RodrÃ­guez Bueno
+    */  
+    @Override
+    public void execute(){
+        while(!finish){
+            switch(state){
+                case IDLE:  //Esperamos a que nos mande los datos del scanner el server
+                    String message;
+                    /*
+                    message = this.receiveMessage("*");
+                    if(message.contains("CRASHED")||message.contains("logout")) 
+                        state = finish;
+                    else{
+                        ...
+                    }
+                    break;
+                    */
+                    message = this.receiveMessage(carName);
+                    
+                    
+                    this.responseObject = Json.parse(message).asObject();
+                    
+                    //Guardamos el array en un int de float mientras nos dan los datos del GPS
+                    int pos = 0;
+                    for (JsonValue j : responseObject.get("scanner").asArray()){
+                        local_scanner[pos] = j.asFloat();
+                        pos++;
+                    }
+                    
+                    state = WAIT_GPS;
+                    break;
+                
+                
+                
+                
+                
+                case WAIT_GPS:    //Esperamos a que nos mande los datos del GPS el GPS Agent
+                    String message;
+                    int x, y;
+                    /*
+                    message = this.receiveMessage("*");
+                    if(message.contains("CRASHED")||message.contains("logout")) 
+                        state = finish;
+                    else{
+                        ...
+                    }
+                    break;
+                    */
+                    message = this.receiveMessage(gpsName);
+                        
+                    this.responseObject = Json.parse(message).asObject();
+                    
+                    x = responseObject.get("x").asInt();
+                    y = responseObject.get("y").asInt();
+                    
+                    //Metemos los datos del scanner dados anteriormente en su posiciÃ³n en map_scanner
+                    int posi = 0;
+                    for (int i = x-1; i <= x+1; i++){
+                        for (int j = y-1; j <= y+1; j++){
+                            map_scanner[i][j] = local_scanner[posi];
+                            posi++;
+                        }
+                    }
+                    
+                    //Avisamos al Agent Car que estamos listos
+                    responseObject = new JsonObject(); //Lo limpiamos
+                    responseObject.add(scannerName, "ok");
+                    message = responseObject.toString();
+                    sendMessage(carName, message);
+                    
+                    state = WAIT_MOVEMENT;
+                    break;
+                    
+                    
+                    
+                    
+                    
+                case WAIT_MOVEMENT: //Esperamos a que nos avise el Movement Agent nos pida los datos del scanner
+                    
+                    String message;
+                    /*
+                    message = this.receiveMessage("*");
+                    if(message.contains("CRASHED")||message.contains("logout")) 
+                        state = finish;
+                    else{
+                        ...
+                    }
+                    break;
+                    */
+                    message = this.receiveMessage(movementName);
+                        
+                    this.responseObject = Json.parse(message).asObject();
+                    
+                    if(message.contains("request")){
+                        responseObject = new JsonObject(); //Lo limpiamos
+                        
+                        //Empaquetamos el array entero en un JSon y se lo enviamos
+                        JsonArray vector = new JsonArray();
+                        for (float [] i : map_scanner){
+                            for (float j : i){
+                                vector.add(j);
+                            }
+                        }
+                        responseObject.add("",vector);
+                        message = responseObject.toString();
+                        this.sendMessage(movementName,message);
+                    }
+                    else{
+                        state = FINISH;
+                    }
+                    
+                    break;
+                    
+                    
+                
+                    
+                
+                case FINISH:    //Matamos al agente
+                    this.finalize();
+                    break;
+            }
+        }
+    }
+    
+    /**
+    * MÃ©todo de finalizaciÃ³n del Agent Scanner
+     * 
+     * @author AarÃ³n RodrÃ­guez Bueno
+    */
+    @Override
+    public void finalize() {
+        super.finalize();
+    }
+}
